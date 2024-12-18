b3bbb9660657eb125192cc385a4ff749ec20ee92
Fulton Browne <fultonbrowne@pm.me>
2024-12-17 20:10:16 -0600

    new theme

--- wezterm.lua
+++ wezterm.lua
@@ -4,7 +4,10 @@ local appearance = require 'appearance'
 local config = wezterm.config_builder()
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 30
-config.window_decorations = 'RESIZE'
+-- config.tab_bar_at_bottom = true
+-- config.enable_tab_bar = false
+-- config.window_decorations = 'RESIZE'
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE|MACOS_FORCE_ENABLE_SHADOW"
 config.window_frame = {
   font = wezterm.font({ family = 'Berkeley Mono', weight = 'Bold' }),
   font_size = 11,
@@ -16,27 +19,4 @@ else
   config.color_scheme = 'Tokyo Night Day'
 end
 
-wezterm.on('update-status', function(window)
-  -- Grab the utf8 character for the "powerline" left facing
-  -- solid arrow.
-  local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-
-  -- Grab the current window's configuration, and from it the
-  -- palette (this is the combination of your chosen colour scheme
-  -- including any overrides).
-  local color_scheme = window:effective_config().resolved_palette
-  local bg = color_scheme.background
-  local fg = color_scheme.foreground
-
-  window:set_right_status(wezterm.format({
-    -- First, we draw the arrow...
-    { Background = { Color = 'none' } },
-    { Foreground = { Color = bg } },
-    { Text = SOLID_LEFT_ARROW },
-    -- Then we draw our text
-    { Background = { Color = bg } },
-    { Foreground = { Color = fg } },
-    { Text = ' ' .. wezterm.hostname() .. ' ' },
-  }))
-end)
 return config
